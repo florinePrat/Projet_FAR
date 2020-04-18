7c56487c24423b153b3dc968811557a0046c59a8
florine <florinepltg64@gmail>
2020-04-18 16:51:23 +0200

    add isValid

---
+++ s2/functionIsValid.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <regex.h>
+
+int isValid(char *pseudo)
+{
+    if (strlen(pseudo)>0 && strlen(pseudo)<10)
+    {
+        if (int regexec("[:alnum:]", pseudo) == 0)
+        {
+            return 1;
+        }
+        else
+        {
+          return 0;
+        }
+    }
+    else
+    {
+      return 0;
+    }
+}
