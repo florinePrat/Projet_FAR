7e92767f2c5dea36f483366e258fdd72e5ef997a
AxelCanton <58460876+AxelCanton@users.noreply.github.com>
2020-05-24 19:43:23 +0200

    Update file_handler.h

--- s4/file_API/file_handler.h
+++ s4/file_API/file_handler.h
@@ -14,15 +14,17 @@ void get_file_name(char *, char *);
 int file_exist(char *);
 
 //Display the files that are stored in the filesToSend directory
-//Only after the "file" command is called
+//Only after the "/file" command is called
 void *thread_files_displaying(void *);
 
 //Parameters : -file's name, -socket of the receiver
-//Send the full file on another thread.
+//Send the file to the socket of the receiver
 void *thread_file_sending(void *);
 
 //Parameters : -file's name, -socket of the receiver , -clientSocket
+//Create a thread executing thread_file_sending for each client present in the channel when they succeedingly connect the the newly created socket
 void *thread_file_accepting(void *);
 
-//No parameters, 
+//Thread that create a new socket and connect to the one of the client that one to send a file
+//Then receives the file and stores it in the dowloadedFile directory
 void *thread_file_receiving(void *);
