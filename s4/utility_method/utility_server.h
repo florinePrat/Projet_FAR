65776afc92190fcfdd1060cb4b5db8044fb148e2
AxelCanton <58460876+AxelCanton@users.noreply.github.com>
2020-05-24 19:00:50 +0200

    Update utility_server.h

--- s4/utility_method/utility_server.h
+++ s4/utility_method/utility_server.h
@@ -13,8 +13,11 @@ extern sem_t overall_sem;
 
 extern sem_t sem_channel[NB_MAX_CHANNEL];
 
+//Initialize availability[i][j] and availabilityChannel[i] to 1, for all i,j>0, i<NB_MAX_CHANNEL and j<NB_MAX_USERS
 void initiate_array();
 
+//Parameter : the number of a channel
+//Change the current admin to the first client available
 void switch_admin(int);
 
 //Method used to factorize the "thread_accept" method
