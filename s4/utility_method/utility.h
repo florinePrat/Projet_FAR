6ccc9cf749882099ee44033b3631e4f5c504b538
AxelCanton <58460876+AxelCanton@users.noreply.github.com>
2020-05-24 19:32:28 +0200

    Update utility.h

--- s4/utility_method/utility.h
+++ s4/utility_method/utility.h
@@ -1,21 +1,39 @@
 extern int nbClientF;
 
 
-//Typing message
-//89 is the max number of caracteres
-//it is under MSG so we have enough bytes to store the pseudonym on server side
+/**
+Parameter : the string used to store the message
+Store the message typed by the user
+**/
 void typing(char *);
 
+/**
+Same than typing, but with a limited number of character in the parameter
+**/
 void typing_with_limit(char*,int);
 
 
-//Check if the chosen channel is valid
-//return 0 if valid, else 1
+/**
+Parameters :
+-the higher channel that the client can join
+-the chosen chosen in a string format
+
+Returns 1 if chosen channel is between 1 and the limit, else 0
+**/
 int channelInvalid(int, char*);
 
+/**
+Parameters :
+-the chosen chosen in a string format
+-the socket of the client
+
+Send to the server the chosen channel if it is a number over 0, or do the necessary to create a channel and send it to the server
+If everything above succeeds, returns 0, else 1
+
+**/
 int channelInvalid_v2(char*,int);
 
-//Check pseudonym
+//Check pseudonym in parameter is valid
 int isValid(char *);
 
 //Store the number of clients connected when the server is sending the corresponding message ("ServerData>..")
@@ -23,5 +41,10 @@ int isValid(char *);
 //nbClient<=80
 void storeNbClient(char *);
 
-
+/**
+Parameter :
+-message in the "file/port/ip" format
+-char array to store the ip
+-char array to store the port
+Separate the initial message and store the part in the concerned char array
 void separate_ip_port(char *,char *, char *);
